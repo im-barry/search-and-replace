8e9b24f0a59b8dda0f9312e1c809bd5a8b2f21c8
Barry Tielkes <barry@instantmagazine.com>
2023-01-23 15:10:35 +0100

    updated setup a bit

--- link-to-local.js
+++ link-to-local.js
@@ -1,19 +1,47 @@
 const changeLines = require('./change-lines');
 // const home = os.homedir();
-const home = process.env.HOME;
-const projects = process.env.PROJECTS;
+// const home = process.env.HOME;
+// const projects = process.env.PROJECTS;
 const packagesToLinkLocal = ['atomic', 'comments'];
 const whereToChange = [
 	{
-		file: `${home}/${projects}/ripley/packages/foleon-core-editor/package.json`,
+		file: `/home/barry/sites/ripley/packages/ui-design-system/package.json`,
 		pathPrefix: '../../../',
 	},
 	{
-		file: `${home}/${projects}/ripley/packages/foleon-core-viewer/package.json`,
+		file: `/home/barry/sites/ripley/packages/image-editor/package.json`,
 		pathPrefix: '../../../',
 	},
 	{
-		file: `${home}/${projects}/comments/package.json`,
+		file: `/home/barry/sites/ripley/packages/foleon-media-library/package.json`,
+		pathPrefix: '../../../',
+	},
+	{
+		file: `/home/barry/sites/ripley/packages/foleon-font-library/package.json`,
+		pathPrefix: '../../../',
+	},
+	{
+		file: `/home/barry/sites/ripley/packages/deprecated-ui-kit/package.json`,
+		pathPrefix: '../../../',
+	},
+	{
+		file: `/home/barry/sites/ripley/maggie/foleon-utils/package.json`,
+		pathPrefix: '../../../',
+	},
+	{
+		file: `/home/barry/sites/ripley/packages/ui-kit/package.json`,
+		pathPrefix: '../../../',
+	},
+	{
+		file: `/home/barry/sites/ripley/packages/foleon-core-editor/package.json`,
+		pathPrefix: '../../../',
+	},
+	{
+		file: `/home/barry/sites/ripley/packages/foleon-core-viewer/package.json`,
+		pathPrefix: '../../../',
+	},
+	{
+		file: `/home/barry/sites/comments/package.json`,
 		pathPrefix: '../',
 	},
 ];
